20f62cb925c71c2359f851905f91376d2208a97a
jackmacha914-hash <jackmacha914@gmail.com>
2025-12-17 13:03:24 +0300

    server.js

--- backend/server.js
+++ backend/server.js
@@ -91,6 +91,8 @@ app.use('/api/quizzes', require('./routes/quizRoutes'));
 app.use('/api/health', require('./routes/health'));
 app.use('/api/transport', require('./routes/transportRoutes'));
 app.use('/api/transport/fees', require('./routes/transportFees'));
+app.use('/api/transport/payments', require('./routes/transportPayments');
+
 
 // -------------------------
 // FRONTEND ROUTES
