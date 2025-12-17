afcde381f9eb01f7e10483678a28e592abf85c54
jackmacha914-hash <jackmacha914@gmail.com>
2025-12-17 06:12:21 +0300

    transport.js

--- backend/frontend_public/js/transport.js
+++ backend/frontend_public/js/transport.js
@@ -1,1 +1,14 @@
+function openTransportModal(id) {
+    const modal = document.getElementById(id);
+    if (modal) {
+        modal.style.display = 'block';
+    }
+}
+
+function closeTransportModal(id) {
+    const modal = document.getElementById(id);
+    if (modal) {
+        modal.style.display = 'none';
+    }
+}
 
